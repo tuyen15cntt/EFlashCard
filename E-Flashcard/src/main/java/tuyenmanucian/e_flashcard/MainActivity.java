615c2dcd8267d6e6cf515bf198bf185c41fdebe4
TUYEN MANUCIAN <tuyenprogl97@gmail.com>
2018-06-07 00:07:27 +0700

    Optimize
    - Clean wrong code
    + Add
    ! Xoa
    * Fix bug

--- E-Flashcard/src/main/java/tuyenmanucian/e_flashcard/MainActivity.java
+++ E-Flashcard/src/main/java/tuyenmanucian/e_flashcard/MainActivity.java
@@ -55,17 +55,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
         getWord();
         addControls();
         addEvents();
-
         //FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-
-
         //readJson(); //init data
         //readFromFB();
+        //read();
         //read();
 
     }
